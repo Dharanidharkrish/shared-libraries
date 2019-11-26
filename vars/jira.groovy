88abe792d6960b830138a51c9ebe8a83e2ed2fae
Dharanidharkrish <55631557+Dharanidharkrish@users.noreply.github.com>
2019-11-26 16:24:34 +0530

    Update jira.groovy

--- vars/jira.groovy
+++ vars/jira.groovy
@@ -1,8 +1,8 @@
-def call(String msg="STAGE FAILED",String id="ULT-0")
-{
-  def issue = [fields: [ project: [key: 'DEV'],
-                       summary: "${msg}",
-                       description: "${id}",
+def call(body){
+
+def issue = [fields: [ project: [key: 'DEV'],
+                       summary: 'Build stage failure',
+                       description: 'BUILD FAILED',
                        issuetype: [name: 'Bug']]]
 def newIssue = jiraNewIssue issue: issue, site: 'JIRA'
 echo newIssue.data.key
